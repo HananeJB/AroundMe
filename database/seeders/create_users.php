307d74dfc27f79ad12490fbbab3c437c1c6d01d2
HananeJB <jabouhanane@gmail.com>
2022-06-17 01:39:41 +0100

    seeds added

--- database/seeders/create_users.php
+++ database/seeders/create_users.php
@@ -25,7 +25,7 @@ class create_users extends Seeder
         ]);
 
         DB::table('users')->insert([
-            'name'=> 'coffer',
+            'name'=> 'coffe',
             'lastname'=> 'Admin',
             'email'=> 'admincoffee@gmail.com',
             'bio'=>"Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
